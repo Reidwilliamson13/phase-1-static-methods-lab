0289b1250754ba29287d05fa5352531597296e72
ReidWilliamson <reidwilliamson2@gmail.com>
2021-11-03 20:12:44 -0400

    commit tree c835eada383da49a6ee4ab662dd1c64195bf2a93

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string1) {
+    return string1.charAt(0).toUpperCase() + string1.slice(1);
+  }
+  static sanitize(string2) {
+    return string2.replace(/[^A-Za-z0-9-' ']/g, "");
+  }
+
+  static titleize(sentence) {
+    let sentenceArray = sentence.split(" ");
+    let uppercasedWordStorage = [];
+    const doNotcapitalize = [
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+    ];
+    for (let i = 0; i < sentenceArray.length; i++) {
+      if (i === 0) {
+        uppercasedWordStorage.push(this.capitalize(sentenceArray[i]));
+      } else {
+        if (doNotcapitalize.includes(sentenceArray[i])) {
+          uppercasedWordStorage.push(sentenceArray[i]);
+        } else {
+          uppercasedWordStorage.push(this.capitalize(sentenceArray[i]));
+        }
+      }
+    }
+    return uppercasedWordStorage.join(" ");
+  }
 }
